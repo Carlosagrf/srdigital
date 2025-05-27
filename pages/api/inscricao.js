8f02ef6caf27f4ef906dfe4e17e380edd77ec4b0
Carlos A. Gonzalez Rodriguez Filho <carlos.agrf@gmail.com>
2025-05-27 11:54:14 -0300

    fix: Adding logs

--- pages/api/inscricao.js
+++ pages/api/inscricao.js
@@ -29,6 +29,7 @@ async function appendToSheet(values) {
 }
 
 export default async function handler(req, res) {
+  console.log("chegou aqui");
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
